39ac4b6bde72a1d8545a043731fb5ce88383a21a
Nicolas Maupu <nmaupu@gmail.com>
2013-10-10 01:19:21 +0200

    Character cannot exit outside of the map

--- src/org/maupu/tiledtest/movingCharacter/TiledTest.java
+++ src/org/maupu/tiledtest/movingCharacter/TiledTest.java
@@ -20,7 +20,7 @@ public class TiledTest extends StateBasedGame {
 	public static void main(String[] args) {
 		try {
 			AppGameContainer container = new AppGameContainer(new TiledTest(GAME_NAME));
-			container.setDisplayMode(640, 640, false);
+			container.setDisplayMode(704, 704, false);
 			container.setVSync(true);
 			container.setShowFPS(false);
 			container.setTargetFrameRate(60);
